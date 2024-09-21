3058699c432a0d86d9939600d102b23ba9992e74
Тимофей Ласкин <timofej.laskin@gmail.com>
2024-09-21 10:40:35 +0500

    Task1

---
+++ Tyuiu.LaskinTA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.LaskinTA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow, World");
+            string name = "Тимофей";
+            int age = 17;
+            Console.WriteLine("Я" + name + ",мне" + age + "Лет");
+            Console.ReadKey();
+        }
+    }
+}
